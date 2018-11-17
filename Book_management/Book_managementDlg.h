ea8e44137cee65b40bdabba0e51ba9c9d9524479
yewl1110 <yewl1110@gmail.com>
2018-11-17 17:16:19 +0900

    Book managament

---
+++ Book_management/Book_managementDlg.h
@@ -0,0 +1,34 @@
+﻿
+// Book_managementDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CBookmanagementDlg 대화 상자
+class CBookmanagementDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CBookmanagementDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_BOOK_MANAGEMENT_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
